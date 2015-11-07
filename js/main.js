4b6a9c6f29cc0cab5916c2468202570d3fff562f
cch5ng <cch5ng@gmail.com>
2015-11-07 08:20:30 -0800

    fix/style: add condition for making opacity 1 when scroll back to the top

--- js/main.js
+++ js/main.js
@@ -22,8 +22,9 @@ function navTransparent(scrolledAmount) {
   var opacity;
   opacity = (1 - fractionToMidBanner) * 0.3;
 
-  if (scrolledAmount > 0 && scrolledAmount <= 200) {
+  if (scrolledAmount > 6 && scrolledAmount <= 200) {
 //                console.log('fractionToMidBanner: ' + fractionToMidBanner);
+//TODO fix so on scroll up to top, all the text displays clearly
     nav.style.backgroundColor = 'rgba(50, 50, 50,' +  opacity + ')';
     brand.style.color = 'rgba(255, 255, 255,' + opacity + ')';
     Array.prototype.forEach.call(menuItems, function(menuItem) {
@@ -41,8 +42,7 @@ function navTransparent(scrolledAmount) {
     Array.prototype.forEach.call(active, function(activeItem) {
         activeItem.style.color = 'rgba(255, 255, 255, 0)';
     });
-    //TODO make text also transparent
-  } else if (scrolledAmount >= 400) {
+  } else if (scrolledAmount >= 400 || scrolledAmount <= 6) {
     nav.style.backgroundColor = 'rgba(50, 50, 50, 0.7)';
     brand.style.color = 'rgba(255, 255, 255, 1)';
     Array.prototype.forEach.call(menuItems, function(menuItem) {
@@ -51,6 +51,5 @@ function navTransparent(scrolledAmount) {
     Array.prototype.forEach.call(active, function(activeItem) {
         activeItem.style.color = 'rgba(255, 255, 255, 1)';
     });
-//TODO this is not getting applied b/c active is currently set at <li> level rather than internal <a> level
   }
 }
